bb108a1fd81e97eafae68c99cc99d43d44273071
dahlian0 <shinoda.lisa.47z@gmail.com>
2022-01-26 00:27:46 +0900

    change input style

--- anotation.py
+++ anotation.py
@@ -9,18 +9,22 @@ import pprint
 
 currentdir = os.getcwd()
 file = glob('*.json')
-for i in file:
-  text_dir= os.path.join(currentdir,str(i))
-  print(text_dir)
-  with open(text_dir) as f:
-    df = json.load(f)
-    for index in range(len(df['regions'])):
-      if df['regions'][index]["tags"] == ['rose2'] :
-        df['regions'][index]["tags"] = ['rose1']
-      if df['regions'][index]["tags"] == ['rose4'] :
-        df['regions'][index]["tags"] = ['rose3']
-    d_update = df
-  with open(text_dir, 'w') as f:
-    json.dump(d_update, f, indent=2, ensure_ascii=False)
+def change(val1, val2):
+  for i in file:
+    text_dir= os.path.join(currentdir,str(i))
+    with open(text_dir) as f:
+      df = json.load(f)
+      for index in range(len(df['regions'])):
+        if df['regions'][index]["tags"] == [val1] :
+          df['regions'][index]["tags"] = [val2]
+      d_update = df
+    with open(text_dir, 'w') as f:
+      json.dump(d_update, f, indent=2, ensure_ascii=False)
+  
+  if __name__ == '__main__':
+    val1 = input('Enter the class name before: ')
+    val2 = input('Enter the class name after: ')
+    change(val1, val2)
+    
   
 
