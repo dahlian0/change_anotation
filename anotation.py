2adce85be379c0399205ae5412a2f3dd184548e0
dahlian0 <shinoda.lisa.47z@gmail.com>
2022-01-26 00:30:19 +0900

    change output direction

--- anotation.py
+++ anotation.py
@@ -8,17 +8,18 @@ from collections import OrderedDict
 import pprint
 
 currentdir = os.getcwd()
-file = glob('*.json')
+file = glob('/data/*.json')
 def change(val1, val2):
   for i in file:
-    text_dir= os.path.join(currentdir,str(i))
+    text_dir= os.path.join(currentdir,"data",str(i))
+    output_dir= os.path.join(currentdir,"output",str(i))
     with open(text_dir) as f:
       df = json.load(f)
       for index in range(len(df['regions'])):
         if df['regions'][index]["tags"] == [val1] :
           df['regions'][index]["tags"] = [val2]
       d_update = df
-    with open(text_dir, 'w') as f:
+    with open(output_dir, 'w') as f:
       json.dump(d_update, f, indent=2, ensure_ascii=False)
   
   if __name__ == '__main__':
